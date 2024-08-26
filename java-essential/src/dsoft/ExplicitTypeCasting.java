818c6eb1739820728d0cf93231d0ff24cdc970fd
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-08-26 15:22:15 +0300

    Explicit Type

---
+++ java-essential/src/dsoft/ExplicitTypeCasting.java
@@ -0,0 +1,13 @@
+package dsoft;
+
+public class ExplicitTypeCasting {
+
+	public static void main(String[] args) {
+		
+		int x = 130;
+		byte b = (byte)x;
+		System.out.println(b);
+
+	}
+
+}
---
+++ java-essential/src/letter_search/LetterSearch.java
@@ -0,0 +1,27 @@
+package letter_search;
+
+import java.util.Scanner;
+
+public class LetterSearch {
+
+	public static void main(String[] args) {
+		
+		//Get text
+		System.out.println("Enter some text:");
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.next();
+		
+		scanner.close();
+		
+		boolean letterFound = false;
+		
+		//Search text for Letter A
+		
+		//for(int i=0; i<text.length(;)) {
+			
+		}
+		
+
+	}
+
+}
