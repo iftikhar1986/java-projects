7eb09aadfb9bb0f68a3ac46ad6903dd0b78ad50f
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-08 16:05:05 +0300

    Quiz App

---
+++ javascript/Simple Quiz App/quiz.js
@@ -0,0 +1,20 @@
+// Quiz questions array
+
+var questions = [
+    {
+        question: 'What is the capital of Qatar?',
+        options: ['Rayyan', 'Doha', 'Cournish', 'Old Airport'],
+        correctAnswer: 'Doha'
+      },
+      {
+        question: 'Which planet is known as the Red Planet?',
+        options: ['Earth', 'Mars', 'Jupiter', 'Venus'],
+        correctAnswer: 'Mars'
+      },
+      {
+        question: 'Who wrote "Hamlet"?',
+        options: ['Mark Twain', 'J.K. Rowling', 'William Shakespeare', 'Charles Dickens'],
+        correctAnswer: 'William Shakespeare'
+      }
+
+];
