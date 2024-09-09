87fe35325706f9e135a826dd2dcbec252211b3a2
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-09 16:56:23 +0300

    Equility

---
+++ javascript/Slearn/equility.js
@@ -0,0 +1,10 @@
+var x = 10
+var y = '10'; 
+
+console.log("x is "+x+" and the data type is: "+typeof x);
+console.log("y is "+y+" and the data type is: "+typeof y);
+
+console.log("Equility Test for x==y:"+(x==y));
+console.log("Equility Test for x===y:"+(x===y));
+
+
---
+++ javascript/Slearn/wellcome.js
@@ -0,0 +1,9 @@
+console.log("wellcome");
+
+let x = 10;
+var y = 20.22; 
+const z = "Iftikhar";
+
+console.log("x is "+x+" and the data type is: "+typeof x);
+console.log("y is "+y+" and the data type is: "+typeof y);
+console.log("z is "+z+" and the data type is: "+typeof z);
