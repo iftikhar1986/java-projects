6514da62e48130b72ff5bd57bd20955cee99342e
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-13 23:10:33 +0300

    Customizing components and directives

--- angular/homes-app/src/app/app.component.ts
+++ angular/homes-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   standalone: true,
@@ -10,12 +12,15 @@ import { Component } from '@angular/core';
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
       <section class="content">
-        
+        <app-home></app-home>
       </section>
     </main>
   
   `,
   styleUrls: ['./app.component.css'],
+  imports: [
+    HomeComponent,
+  ],
 })
 export class AppComponent {
   title = 'homes';
