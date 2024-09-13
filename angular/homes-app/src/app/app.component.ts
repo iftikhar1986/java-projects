210d3582687021baacdc283209b9f5c0f14de570
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-13 22:20:01 +0300

    Components in Angular

--- angular/homes-app/src/app/app.component.ts
+++ angular/homes-app/src/app/app.component.ts
@@ -3,17 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `
-  
-  <main>
-    <header class="brand-name">
-      <img  class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-  </main>
-  
-  
-  
-  `,
+  template: `<h1>Hello Universe!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
