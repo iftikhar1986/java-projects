02e36b40ae5acb23e564e6e34fa23aaa1d2fc7db
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-14 01:06:22 +0300

    Services

--- angular/homes-app/src/app/details/details.component.ts
+++ angular/homes-app/src/app/details/details.component.ts
@@ -1,23 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works! {{ housingLocationId }}
-    </p>
+    <article class="listing-description">
+      <img class="listing-photo" [src]="housingLocation?.photo"/>
+      <section>
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2> 
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}} </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location </h2>
+      </section>
+    </article> 
   `,
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
   route:ActivatedRoute = inject(ActivatedRoute); 
-  housingLocationId = 0;
-
+  housingService  = inject(HousingService);
+  housingLocationId: HousingLocation | undefined;
   constructor() {
-    this.housingLocationId = this.route.snapshot.params['id'];
+    const  housingLocationId = this.route.snapshot.params['id'];
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 }
