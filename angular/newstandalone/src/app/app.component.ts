a82ecfdaf8e32fd69cda075a00092d2ff83050ca
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-21 17:35:16 +0300

    project

--- angular/newstandalone/src/app/app.component.ts
+++ angular/newstandalone/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
+import { reverse } from 'dns';
+import { ReversePipe } from './custom/reverse.pipe';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, HomeComponent, CommonModule, ReversePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'newstandalone';
+  title = 'newstandalone-Angular 18';
+  subtitle = 'For moi';
+  todayDate = new Date(); //todayDate = new Date('2023-05-01');
+  salary = 1000;
 }
