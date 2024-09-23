a745a18c58de61306d32167853665877407294cd
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-09-23 09:35:17 +0300

    new code added

--- angular/services-http-forms-routing/src/app/model/product.ts
+++ angular/services-http-forms-routing/src/app/model/product.ts
@@ -1,2 +1,16 @@
 export class Product {
+
+
+    constructor(
+        public id:number=0,
+        public name: string ='',
+        public price: number=100,
+        public description: string='',
+        public inStock:boolean=true,
+        public imagePath:string='',
+      ) {
+       
+      }
+
+
 }
