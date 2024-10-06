ed8a1c9549da0aa2dddbc5e57bb598a98541ce53
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-10-06 08:32:40 +0300

    two componenets created for client-creation and client-meetings

---
+++ angular/client-management-app/src/app/client-creation/client-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-creation.component.html',
+  styleUrl: './client-creation.component.css'
+})
+export class ClientCreationComponent {
+
+}
---
+++ angular/client-management-app/src/app/client-meetings/client-meetings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-meetings',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-meetings.component.html',
+  styleUrl: './client-meetings.component.css'
+})
+export class ClientMeetingsComponent {
+
+}
